3bcf920c761b3406e5eba77199627c91c60fb0c0
Guillermo24-10 <pgaugusto24@gmail.com>
2026-08-05 17:16:37 -0500

    ultimo_cambios_protegidos

--- Productos.Api/obj/Debug/net10.0/Productos.Api.AssemblyInfo.cs
+++ Productos.Api/obj/Debug/net10.0/Productos.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Productos.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd1e5d1334509ffd53a0118b6b0527e17e54740")]
 [assembly: System.Reflection.AssemblyProductAttribute("Productos.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Productos.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
